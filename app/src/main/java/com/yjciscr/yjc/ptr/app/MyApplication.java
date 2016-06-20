6f3f788ddb63ae362c3ed54410611c1aa345f238
yjciscr <yjciscr@hotmail.com>
2016-06-20 19:46:15 +0800

    grateapplication

---
+++ app/src/main/java/com/yjciscr/yjc/ptr/app/MyApplication.java
@@ -0,0 +1,13 @@
+package com.yjciscr.yjc.ptr.app;
+
+import android.app.Application;
+
+/**
+ * Created by YJC on 2016/6/20.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
